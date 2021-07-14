36af4af22e048be5ab4384d62be9e038b3b60b07
louisvincelli <79131518+louisvincelli@users.noreply.github.com>
2021-07-13 23:32:59 -0700

    profileroutes

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const loginRoutes = require('./login-routes');
-//const profileRoutes = require('./profile-routes');
+const profileRoutes = require('./profile-routes');
 
 router.use('/', homeRoutes);
 router.use('/login', loginRoutes);
-//router.use('/profile', profileRoutes);
+router.use('/profile', profileRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
